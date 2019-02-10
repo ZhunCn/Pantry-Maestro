b35b4d18daca0e60a4f95efa4a607a31abe00ae3
Owen Shen <shenowen36@gmail.com>
2019-02-10 17:09:52 -0500

    Add verification methods for input checking for Login/Register

--- src/frontend/containers/LoginPage/index.js
+++ src/frontend/containers/LoginPage/index.js
@@ -4,6 +4,42 @@ import ReactDOM from 'react-dom';
 import GenericNavigationBar from '@/components/GenericNavigationBar';
 import './styles.scss';
 
+let validUser = function verifyUser(username){
+  //check for valid lengths
+  if(username.getLength == 0){
+    return false;
+  }
+  
+  if(username.getLength > 32){
+    return false;
+  }
+
+  return true;
+}
+
+let validPass = function verifyPass(password){
+  //check for valid lengths
+  if(password.getLength == 0){
+    return false;
+  }
+  
+  if(password.getLength > 32){
+    return false;
+  }
+  
+  //check that password has letters AND numbers
+
+  //modular regex design
+  var letters = /^[a-zA-Z]+$/;
+  var numbers = /^[0-9]+$/;
+
+  if(!password.value.match(letters) || !password.value.match(numbers)){
+    return false;
+  }
+
+  return true;
+}
+
 export default class Login extends React.Component {
   render() {
     return (
