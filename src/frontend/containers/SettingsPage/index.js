8481f8b5e6cb27ab9fe263ba5119a8807ec69772
Owen Shen <shenowen36@gmail.com>
2019-02-10 18:01:46 -0500

    add input checking on Settings similar to Login/Register

--- src/frontend/containers/SettingsPage/index.js
+++ src/frontend/containers/SettingsPage/index.js
@@ -4,6 +4,43 @@ import ReactDOM from 'react-dom';
 import GenericNavigationBar from '@/settings/GenericNavigationBar';
 import './styles.scss';
 
+//add input checking on Settings similar to Login/Register
+let validUser = function verifyUser(username){
+  //check for valid lengths
+  if(username.getLength == 0){
+    return false;
+  }
+  
+  if(username.getLength > 32){
+    return false;
+  }
+
+  return true;
+}
+
+let validPass = function verifyPass(password){
+  //check for valid lengths
+  if(password.getLength == 0){
+    return false;
+  }
+  
+  if(password.getLength > 32){
+    return false;
+  }
+  
+  //check that password has letters AND numbers
+
+  //modular regex design
+  var letters = /^[a-zA-Z]+$/;
+  var numbers = /^[0-9]+$/;
+
+  if(!password.value.match(letters) || !password.value.match(numbers)){
+    return false;
+  }
+
+  return true;
+}
+
 export default class Settings extends React.Component {
   render() {
     return (
