669e7659c48f83631733e389451bbd5943cbc080
Ayush <sinha46@purdue.edu>
2019-02-10 16:16:41 -0500

    Changed the a-tag links on the Navigation Bar to React Links.

--- src/frontend/components/GenericNavigationBar/index.js
+++ src/frontend/components/GenericNavigationBar/index.js
@@ -8,11 +8,11 @@ export default class GenericNavigationBar extends React.Component {
   render() {
     return (
       <Menu>
-          <a className="menu-item" href="/"><img src={prof} alt = "Profile" /></a>
-          <a className="menu-item" href="/">Home</a>
-          <a className="menu-item" href="/about">About</a>
-          <a className="menu-item" href="/login">Login</a>
-          <a className="menu-item" href="#">Blank</a>
+          <Link to="/"><img src={prof} alt = "Profile" /></Link>
+          <Link to="/">Home</Link>
+          <Link to="/about">About</Link>
+          <Link to="/login">Login</Link>
+          <Link to="/#">Blank</Link>
         </Menu>
       );
   }
