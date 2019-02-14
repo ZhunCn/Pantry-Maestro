73e0af276c49d22735680c40889fd23d3a4d7c7d
Sam Lin <lin854@purdue.edu>
2019-02-14 00:12:40 -0500

    Created rudimentary UI for Settings.

--- src/frontend/containers/SettingsPage/index.js
+++ src/frontend/containers/SettingsPage/index.js
@@ -10,7 +10,7 @@ let validUser = function verifyUser(username){
   if(username.getLength == 0){
     return false;
   }
-  
+
   if(username.getLength > 32){
     return false;
   }
@@ -23,11 +23,11 @@ let validPass = function verifyPass(password){
   if(password.getLength == 0){
     return false;
   }
-  
+
   if(password.getLength > 32){
     return false;
   }
-  
+
   //check that password has letters AND numbers
 
   //modular regex design
@@ -47,7 +47,42 @@ export default class Settings extends React.Component {
       <div>
         <GenericNavigationBar/>
         <div class="Content">
-        <p>Settings component!</p>
+        <h2>Settings</h2>
+	       <div class="Total">
+	        <div class="Left">
+		        <form>
+			         <p>First name:</p>
+  			          <input type="text" name="firstname"/>
+			         <p>Last name:</p>
+  			          <input type="text" name="lastname"/>
+			         <p>Username:</p>
+  			          <input type="text" name="username"/>
+			         <p>Change email:</p>
+  			          <input type="text" name="email"/>
+			         <p>Workspace Information:</p>
+		           <p><i>Workspace Info Here</i></p>
+  			       <button>Leave Workspace</button>
+		        </form>
+	        </div>
+	        <div class="Right">
+		        <form>
+			         <p><strong>Change Password:</strong></p>
+			         <p>Current Password:</p>
+  			          <input type="text" name="firstname"/>
+			         <p>New Password</p>
+  			          <input type="text" name="lastname"/>
+		           <p>Confirm Password</p>
+  			       <input type="text" name="username"/>
+               <button>Change Password</button>
+		        </form>
+          </div>
+	    </div>
+      <div class="Footer">
+      <div class="flex">
+		  <button class="Save">Save</button>
+		  <button class="Discard">Discard</button>
+	     </div>
+       </div>
         </div>
       </div>
     );
