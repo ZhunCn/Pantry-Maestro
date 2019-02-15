20df3865eeb92132c6d4aa360b418464ed828ee8
Amarto Pramanik <pramanik@purdue.edu>
2019-02-15 02:03:49 -0500

    Added capability to add items to the server.

--- src/frontend/components/AddChangeItemComponent/index.js
+++ src/frontend/components/AddChangeItemComponent/index.js
@@ -17,14 +17,56 @@ export default class AddChangeItemComponent extends React.Component {
     }   
 
     addQuantity = (e) => {
+        e.preventDefault();
+
         this.setState((prevState) => ({
             quantities: [...prevState.quantities, {"date":"", "quantity": 0}],
         }))
     }
 
-    handleSubmit = (e) => { e.preventDefault() }
+    // parseData(unparsedData) {
+    //     // "name": "Starburst",
+    //     // "quantities": {
+    //     //     "01/01/2019": 12,
+    //     //     "02/13/2020": 23,
+    //     //     "12/22/2019": 5
+    //     // }
+    //     let parsedData = {
+    //         "name": unparsedData.name,
+    //         "quantities": {}
+    //     }
+    //     for (let i = 0; i < unparsedData.quantities.length; i++) {
+    //         parsedData.quantities[unparsedData.quantities[i].date] = parseInt(unparsedData.quantities[i].quantity);
+    //     }
+    //     console.log(parsedData);
+    //     return parsedData;
+
+    // }
+
+    handleSubmit = (e) => { 
+        e.preventDefault();
+
+        let parsedData = {
+            "name": this.state.name,
+            "quantities": {}
+        }
+        for (let i = 0; i < this.state.quantities.length; i++) {
+            if (this.state.quantities[i].date != "") { 
+                parsedData.quantities[this.state.quantities[i].date] = parseInt(this.state.quantities[i].quantity);
+            }
+        }
+        console.log(parsedData);
+        axios.post(`/api/workspaces/${workspaceID}/inventory`, parsedData).then(res => {
+            console.log(res.data);
+        })
+    }
+
+
+   
  
     handleChange = (e) => {
+        e.preventDefault();
+
         if (["date", "quantity"].includes(e.target.className)) {
             let quantities = [...this.state.quantities]
             quantities[e.target.dataset.id][e.target.className] = e.target.value
@@ -34,16 +76,16 @@ export default class AddChangeItemComponent extends React.Component {
         }
     }
 
-    calendarHandleChange(date) {
-        let quantities = [...this.state.quantities]
-        quantities[date.id][date.className] = date.value
-    }
+    // calendarHandleChange(date) {
+    //     let quantities = [...this.state.quantities]
+    //     quantities[date.id][date.className] = date.value
+    // }
 
     render() {
         let {name, quantities} = this.state
         return (
             <div className="AddChangeItemForm">
-            <form onSubmit={this.handleSubmit} onChange={this.handleChange}>
+            <form onChange={this.handleChange}>
                 <h2>Add new food items</h2>
                 <label htmlFor="name">Name:</label>
                 <input type="text" name="name" id="name"/>
@@ -54,19 +96,13 @@ export default class AddChangeItemComponent extends React.Component {
                         return (
                             <div key={idx}>
                                 <label htmlFor={dateId}>{`Expiration #${idx + 1}:  `}</label>
-                                {/* <input
+                                <input
                                     type="text"
                                     name={dateId}
                                     data-id={idx}
                                     id={dateId}
                                     className="date"
                                     placeholder="Enter Expiration"
-                                /> */}
-                                <DatePicker 
-                                    name={dateId}
-                                    id={dateId}
-                                    className="date"
-
                                 />
                                 <label htmlFor={quantityId}>Quantity:  </label>
                                 <input 
@@ -81,7 +117,7 @@ export default class AddChangeItemComponent extends React.Component {
                         )
                     })
                 }
-                <input type="submit" value="Submit"/>
+                <input type="button" value="Submit" onClick={this.handleSubmit}/>
             </form>
             </div>
         );
