d626a4f16e39f5803463842a0c7816d0d0396b1f
Ayush <sinha46@purdue.edu>
2019-03-01 21:36:12 -0500

    Added tabs to the Analytics Page to seperate out the three sections.

--- src/frontend/containers/AnalyticsPage/index.js
+++ src/frontend/containers/AnalyticsPage/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import GenericNavigationBar from '@/components/GenericNavigationBar';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import "react-tabs/style/react-tabs.css";
+//import Tabs from '@/components/AnalyticsTabs/Tabs';
 import './styles.scss';
 
 export default class Analytics extends React.Component {
@@ -10,7 +13,28 @@ export default class Analytics extends React.Component {
       <div class="analyticsPage">
         <GenericNavigationBar/>
         <div class="Content">
-        <p>Analytics Page !! Wowowowowow!!!</p>
+        <h1>Analytics</h1>
+        <Tabs>
+
+          <TabList>
+            <Tab><font face="Comic Sans MS" size="4" color="grey"><b>Expiration Analytics</b></font></Tab>
+            <Tab><font face="Comic Sans MS" size="4" color="grey"><b>Quantity Analytics</b></font></Tab>
+            <Tab><font face="Comic Sans MS" size="4" color="grey"><b>Popularity Analytics</b></font></Tab>
+          </TabList>
+
+          <TabPanel>
+            What?<br/>
+            Your Milk is evolving!<br/>
+            Congratulations! Your Milk has evolved into Expired Milk!
+          </TabPanel>
+          <TabPanel>
+            <em>Why?</em> Because Quantity over Quality.
+          </TabPanel>
+          <TabPanel>
+            Was gonna make a joke about AnalYtics.. but maybe not..
+          </TabPanel>
+
+        </Tabs>
         </div>
       </div>
     );
