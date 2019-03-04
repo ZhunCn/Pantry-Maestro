b1e786b72e63ebfd81f5e0fd408ee2ada60fd47f
ZhunCn <randyzhunc@gmail.com>
2019-03-03 21:34:23 -0500

    Add Invite Page with basic UI
    Accept/Decline appears when loginToken in present. Otherwise, prompt with registration UI.

--- src/frontend/router/index.js
+++ src/frontend/router/index.js
@@ -7,6 +7,7 @@ import About from '@/containers/AboutPage'
 import Analytics from '@/containers/AnalyticsPage'
 import Login from '@/containers/LoginPage'
 import Register from '@/containers/RegisterPage'
+import Invite from '@/containers/InvitePage'
 import Inventory from '@/containers/InventoryPage'
 import Settings from "@/containers/SettingsPage";
 import Workspace from "@/containers/WorkspacePage";
@@ -20,6 +21,7 @@ const Router = () => {
         <Route path="/analytics" component={Analytics} />
         <Route path="/login" component={Login} />
         <Route path="/register" component={Register} />
+        <Route path="/invite" component={Invite} />
         <Route path="/inventory" component={Inventory} />
         <Route path="/settings" component={Settings} />
         <Route path="/workspace" component={Workspace} />
