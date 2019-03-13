554adf7c2b91de59788c9ab2bed076603c529ec6
Amarto <cncgamerx@gmail.com>
2019-03-13 02:48:39 -0600

    Revamped login and register pages.
    Styled using semantic-ui, and updated code to utilized React better.
    Now uses toasts to show different error messages as well as the fields changing.
    Login and register buttons are disabled until fields are filled with something.

--- src/frontend/containers/LoginPage/index.js
+++ src/frontend/containers/LoginPage/index.js
@@ -1,14 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {Link, Redirect} from 'react-router-dom';
+import { Link, Redirect } from 'react-router-dom';
 import axios from 'axios';
-
-import {authorize} from '@/utils';
+import { Button, Form, Grid, Header, Image, Message, Segment } from 'semantic-ui-react'
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { authorize } from '@/utils';
 
 import './styles.scss';
 
 
 export default class Login extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+      usernameError: false,
+      passwordError: false
+    }
+    this.onInputChange = this.onInputChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.verifyUser = this.verifyUser.bind(this);
+    this.verifyPass = this.verifyPass.bind(this);
+  }
+
   verifyUser(username) {
     /**
       * Inputted "Username" for login can either be actual Username
@@ -19,142 +35,139 @@ export default class Login extends React.Component {
     //console.log('Inputted username (verifyUser): ', username);
 
     //check for valid lengths
-    if(username.length == 0){
-      return false;
+    if (username.length == 0 || username.length > 32) {
+      this.setState({ usernameError: true });
+      toast("Invalid username! Usernames must be less than 32 characters in length", { type: "error" });
+      return;
+    } else {
+      this.setState({ usernameError: false });
     }
-
-    if(username.length > 32){
-      var emailregex = /[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}/;
-
-      //check if username looks like an email
-      if(username.match(emailregex) == null){
-        return false;
-      }
-      return true;
-    }
-
-    return true;
   }
 
   verifyPass(password) {
     //console.log('Inputted password (verifyPass): ', password);
 
     //check for valid lengths
-    if(password.length < 6){
-      return false;
+    if (password.length <= 6) {
+      this.setState({ passwordError: true })
+      // toast("Invalid password! Make sure your password contains at least 1 number and is longer than 6 characters", { type: "error" });
+      return;
     }
-
-    if(password.length > 32){
-      return false;
-    }
-
     //check that password has letters AND numbers
-
     //modular regex design
-    var letters = /[a-zA-Z]+/;
-    var numbers = /[0-9]+/;
+    let letters = /[a-zA-Z]+/;
+    let numbers = /[0-9]+/;
 
-    if(password.match(letters) == null || password.match(numbers) == null) {
-      return false;
+    if (password.match(letters) == null || password.match(numbers) == null) {
+      this.setState({ passwordError: true })
+      // toast("Invalid password! Make sure your password contains at least 1 number and is longer than 6 characters", { type: "error" });
+      return;
     }
 
-    return true;
+    this.setState({ passwordError: false })
   }
 
-
-  loginProcedure() {
-    document.getElementById("usernamePrompt").textContent = "";
-    document.getElementById("passwordPrompt").textContent = "";
+  handleSubmit = (e) => {
+    e.preventDefault();
+    // document.getElementById("usernamePrompt").textContent = "";
+    // document.getElementById("passwordPrompt").textContent = "";
 
 
     // Grab username and password from field
-    var username = document.getElementById("usernameField").value;
-    var password = document.getElementById("passwordField").value;
-    console.log('Inputted username (loginProcedure): ', username);
-    console.log('Inputted password (loginProcedure): ', password);
+    console.log('Inputted username (loginProcedure): ', this.state.username);
+    console.log('Inputted password (loginProcedure): ', this.state.password);
 
 
-    if ((this.verifyUser(username)) == true) {
+    this.verifyUser(this.state.username);
+    this.verifyPass(this.state.password);
+
+
+    if (!this.state.usernameError) {
       console.log('Valid username!');
 
-      if ((this.verifyPass(password)) == true) {
+      if (!this.state.passwordError) {
         console.log('Valid password!');
 
         // Connect with backend to verify login information is correct
         axios.post('/api/auth/login', {
-          'username': username,
-          'password': password
+          'username': this.state.username,
+          'password': this.state.password
         }).then(res => {
           console.log(res.data);
-          localStorage.setItem('loginToken', res.data.token);
-          document.getElementById("successParagraph").textContent = "Successfully logged in!";
-          document.getElementById("successParagraph").style = "color:green;";
-
-          this.props.history.push('/inventory');
-        })
-        .catch((error) => {
-          console.log(error.data);
+          if (res.data.error == "Invalid credentials") {
+            this.setState({ passwordError: true });
+            this.setState({ usernameError: true });
+            toast("Username/Password is incorrect. Please try again.", { type: "error" });
+          } else {
+            localStorage.setItem('loginToken', res.data.token);
+            this.props.history.push('/inventory');
+          }
+        }).catch((error) => {
+          console.log(error);
+          this.setState({ passwordError: true });
+          this.setState({ usernameError: true });
+          toast("Username/Password is incorrect. Please try again.", { type: "error" });
         });
       } else {
         console.log('Invalid password!');
-        document.getElementById("passwordPrompt").textContent = "(Invalid password!)";
-        document.getElementById("passwordPrompt").style = "color:red;";
       }
     } else {
       console.log('Invalid username!');
-      document.getElementById("usernamePrompt").textContent = "(Invalid username!)";
-      document.getElementById("usernamePrompt").style = "color:red;";
     }
 
   }
 
-  enterPressedOnPassword() {
-    if (event.keyCode === 13) {
-      document.getElementById("loginButton").click();
-    }
-  }
 
-  handlePasswordChange(event) {
-    if (this.verifyPass(event.target.value)) {
-      document.getElementById("passwordPrompt").textContent = "";
-    }
-  }
-
-  handleUsernameChange(event) {
-    if (this.verifyUser(event.target.value)) {
-      document.getElementById("usernamePrompt").textContent = "";
-    }
+  onInputChange(event) {
+    console.log(event);
+    this.state.setState
+    this.setState({ [event.target.name]: event.target.value })
   }
 
   render() {
     if (authorize()) {
       return (
-        <Redirect to="/"/>
+        <Redirect to="/" />
       )
     }
 
     return (
-      <div class="loginPage">
-        <div class="Content">
-        <center>
-          <h2>Login</h2>
-          <form>
-            <p>Username or Email<p id="usernamePrompt"></p></p>
-            <input type="text" name="usernameField" id="usernameField" onChange={(event) => {this.handleUsernameChange(event)}} placeholder="Username"></input><br></br>
-
-            <p>Password<p id="passwordPrompt"></p></p>
-            <input type="password" name="passwordField" id="passwordField" onChange={(event) => {this.handlePasswordChange(event)}} onKeyDown={(e) => this.enterPressedOnPassword()} placeholder="Password"></input>
-          </form>
-          <p><button class="button" id="loginButton" onClick={(e) => this.loginProcedure()}>Log In</button><p id="successParagraph"></p></p>
-          <p><Link id="forgotPasswordLink" to="/login/#">Forgot Password?</Link></p>
-          <Link to="/register"><button class="button" id="signUpButton">New to Pantry Maestro? Sign Up</button></Link>
-        </center>
-        <div class="Footer">
-          <div class="Flex">
-          </div>
-        </div>
-
-        </div>
+      <div className='login-form'>
+        <ToastContainer autoClose={3000} />
+        <Grid textAlign='center' style={{ height: '100%' }} verticalAlign='middle' className='middle aligned'>
+          <Grid.Column style={{ maxWidth: 450 }}>
+            <Header as='h2' color='black' textAlign='center'>
+              Log in to your account
+              </Header>
+            <Form size='large' onSubmit={(e) => { this.handleSubmit(e) }}>
+              <Segment stacked>
+                <Form.Input
+                  name="username"
+                  fluid icon='user'
+                  iconPosition='left'
+                  placeholder='Username'
+                  value={this.state.username}
+                  onChange={this.onInputChange}
+                  error={this.state.usernameError} />
+                <Form.Input
+                  name="password"
+                  fluid icon='lock'
+                  iconPosition='left'
+                  placeholder='Password'
+                  type='password'
+                  value={this.state.password}
+                  onChange={this.onInputChange}
+                  error={this.state.passwordError} />
+                <Form.Button fluid size='large' type='submit' disabled={!this.state.username || !this.state.password}>
+                  Login
+                </Form.Button>
+              </Segment>
+            </Form>
+            <Message>
+              <Link to="/register"><Button id="signUpButton">New to Pantry Maestro? Sign Up</Button></Link>
+            </Message>
+          </Grid.Column>
+        </Grid>
       </div>
     );
   }
