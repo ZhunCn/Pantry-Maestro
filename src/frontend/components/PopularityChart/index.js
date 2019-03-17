9e0138e97df39dd467696f873107ddc64cc21f21
Ayush <sinha46@purdue.edu>
2019-03-17 03:32:58 -0400

    Added charts with dummy values for popularity analytics.

---
+++ src/frontend/components/PopularityChart/index.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import {Bar, Line, Pie} from 'react-chartjs-2';
+
+class PopularityChart extends Component{
+  constructor(props){
+    super(props);
+    this.state = {
+      chartData:props.chartData
+    }
+  }
+
+  static defaultProps = {
+    displayTitle:true,
+    displayLegend: true,
+    legendPosition:'right',
+    pantry:'Hooters'
+  }
+
+  render(){
+    return (
+      <div className="chart">
+        <Bar
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Most Popular Food Items In '+this.props.pantry,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+
+        <Line
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Most Popular Food Items In '+this.props.pantry,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+
+        <Pie
+          data={this.state.chartData}
+          options={{
+            title:{
+              display:this.props.displayTitle,
+              text:'Most Popular Food Items In '+this.props.pantry,
+              fontSize:25
+            },
+            legend:{
+              display:this.props.displayLegend,
+              position:this.props.legendPosition
+            }
+          }}
+        />
+      </div>
+    )
+  }
+}
+
+export default PopularityChart;
--- src/frontend/containers/AnalyticsPage/index.js
+++ src/frontend/containers/AnalyticsPage/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import PopularityChart from '@/components/PopularityChart';
 
 import GenericNavigationBar from '@/components/GenericNavigationBar';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
@@ -8,6 +9,44 @@ import "react-tabs/style/react-tabs.css";
 import './styles.scss';
 
 export default class Analytics extends React.Component {
+
+  componentWillMount(){
+    this.getChartData();
+  }
+
+  getChartData(){
+    // Ajax calls here
+    this.setState({
+      chartData:{
+        labels: ['Ramen', 'Frozen Pizza', 'Milk', 'Macaroni & Cheese', 'Rice', 'Ice Cream', 'Chocolate Ice Cream'],
+        datasets:[
+          {
+            label:'Food Item',
+            data:[
+              617,
+              181,
+              153,
+              105,
+              109,
+              80,
+              1000
+            ],
+            backgroundColor:[
+              'rgba(255, 99, 132, 0.6)',
+              'rgba(54, 162, 235, 0.6)',
+              'rgba(255, 206, 86, 0.6)',
+              'rgba(75, 192, 192, 0.6)',
+              'rgba(153, 102, 255, 0.6)',
+              'rgba(255, 159, 64, 0.6)',
+              'rgba(139, 69, 19, 0.6)'
+            ]
+          }
+        ]
+      }
+    });
+  }
+
+
   render() {
     return (
       <div class="analyticsPage">
@@ -17,9 +56,9 @@ export default class Analytics extends React.Component {
         <Tabs>
 
           <TabList>
-            <Tab><font face="Comic Sans MS" size="4" color="grey"><b>Expiration Analytics</b></font></Tab>
-            <Tab><font face="Comic Sans MS" size="4" color="grey"><b>Quantity Analytics</b></font></Tab>
-            <Tab><font face="Comic Sans MS" size="4" color="grey"><b>Popularity Analytics</b></font></Tab>
+            <Tab><font face="Futura" size="4" color="grey"><b>Expiration Analytics</b></font></Tab>
+            <Tab><font face="Futura" size="4" color="grey"><b>Quantity Analytics</b></font></Tab>
+            <Tab><font face="Futura" size="4" color="grey"><b>Popularity Analytics</b></font></Tab>
           </TabList>
 
           <TabPanel>
@@ -31,7 +70,8 @@ export default class Analytics extends React.Component {
             <em>Why?</em> Because Quantity over Quality.
           </TabPanel>
           <TabPanel>
-            Was gonna make a joke about AnalYtics.. but maybe not..
+            Was gonna make a joke about AnalYtics.. but maybe not..<br/>
+            <PopularityChart chartData={this.state.chartData} pantry="/Insert Workspace Name Here/" legendPosition="bottom"/>
           </TabPanel>
 
         </Tabs>
