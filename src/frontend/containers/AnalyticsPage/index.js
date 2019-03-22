b109cc9c65845a0a4873046a3e3c38e01456d7d5
Ayush <sinha46@purdue.edu>
2019-03-22 04:18:22 -0400

    Parsed data into the popularity analytics.

--- src/frontend/containers/AnalyticsPage/index.js
+++ src/frontend/containers/AnalyticsPage/index.js
@@ -6,19 +6,61 @@ import ExpirationChart from '@/components/ExpirationChart';
 import GenericNavigationBar from '@/components/GenericNavigationBar';
 import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
 import "react-tabs/style/react-tabs.css";
+import axios from 'axios';
+
 //import Tabs from '@/components/AnalyticsTabs/Tabs';
 import './styles.scss';
 
 export default class Analytics extends React.Component {
 
   componentWillMount(){
-    this.getChartData();
+    this.getDemoChartData();
+    this.getPopChartData();
+    //this.getQuantChartData();
+    //this.getExpChartData();
+  }
+  getPopChartData(){
+    let userLoginToken = localStorage.getItem("loginToken");
+    let workspaceID = localStorage.getItem("currWorkspaceID");
+    axios.get(`/api/workspaces/${workspaceID}/analytics/quantities/top`, { headers: { "Authorization" : `${userLoginToken}` } }).then(res => {
+        console.log(workspaceID);
+        console.log(userLoginToken);
+        //console.log(res.analytics);
+      if(res.data.analytics.inventory.items.length != 3){
+        console.log("hi0");
+        console.log(res.data.analytics.inventory.items);
+      }else{
+        console.log("hi");
+        var labellist = [res.data.analytics.inventory.items[0].name,res.data.analytics.inventory.items[1].name,res.data.analytics.inventory.items[2].name];
+        var qval = [res.data.analytics.inventory.items[0].total,res.data.analytics.inventory.items[1].total,res.data.analytics.inventory.items[2].total];
+        this.setState({
+          chartPopData:{
+            labels: labellist,
+            datasets:[
+              {
+                label: 'Food Item',
+                data: qval,
+                backgroundColor:[
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)'
+                ]
+              }
+            ]
+          }
+        });
+      }
+    });
   }
 
-  getChartData(){
+  getDemoChartData(){
     // Ajax calls here
     this.setState({
-      chartData:{
+      chartDemoData:{
         labels: ['Ramen', 'Frozen Pizza', 'Milk', 'Macaroni & Cheese', 'Rice', 'Ice Cream', 'Chocolate Ice Cream'],
         datasets:[
           {
@@ -66,15 +108,15 @@ export default class Analytics extends React.Component {
             What?<br/>
             Your Milk is evolving!<br/>
             Congratulations! Your Milk has evolved into Expired Milk!
-            <ExpirationChart chartData={this.state.chartData} pantry="/Insert Workspace Name Here/" legendPosition="bottom"/>
+            <ExpirationChart chartData={this.state.chartDemoData} pantry="/Insert Workspace Name Here/" legendPosition="bottom"/>
           </TabPanel>
           <TabPanel>
             <em>Why?</em> Because Quantity over Quality.
-            <QuantityChart chartData={this.state.chartData} pantry="/Insert Workspace Name Here/" legendPosition="bottom"/>
+            <QuantityChart chartData={this.state.chartDemoData} pantry="/Insert Workspace Name Here/" legendPosition="bottom"/>
           </TabPanel>
           <TabPanel>
             Was gonna make a joke about AnalYtics.. but maybe not..<br/>
-            <PopularityChart chartData={this.state.chartData} pantry="/Insert Workspace Name Here/" legendPosition="bottom"/>
+            <PopularityChart chartData={this.state.chartPopData} pantry="/Insert Workspace Name Here/" legendPosition="bottom"/>
           </TabPanel>
 
         </Tabs>
