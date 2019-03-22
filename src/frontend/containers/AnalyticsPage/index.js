5835529e0885f12ecc8fb8652d27646f106dafb7
Ayush <sinha46@purdue.edu>
2019-03-22 14:41:24 -0400

    Parsed data for expiration analytics.

--- src/frontend/containers/AnalyticsPage/index.js
+++ src/frontend/containers/AnalyticsPage/index.js
@@ -14,27 +14,28 @@ import './styles.scss';
 export default class Analytics extends React.Component {
 
   componentWillMount(){
+    this.getExpChartData();
     this.getDemoChartData();
     this.getPopChartData();
     this.getQuantChartData();
-    //this.getExpChartData();
+
   }
   getPopChartData(){
     let userLoginToken = localStorage.getItem("loginToken");
     let workspaceID = localStorage.getItem("currWorkspaceID");
     axios.get(`/api/workspaces/${workspaceID}/analytics/quantities/top`, { headers: { "Authorization" : `${userLoginToken}` } }).then(res => {
-        console.log(workspaceID);
-        console.log(userLoginToken);
+        //console.log(workspaceID);
+        //console.log(userLoginToken);
         //console.log(res.analytics[0].name);
-        console.log(res.data);
+        //console.log(res.data);
 
 
       //if(res.data.analytics.length != 3){
       //  console.log("hi0");
       //  console.log(res.data.analytics);
       //}else{
-        console.log("hi");
-        console.log(res.data.analytics);
+        //console.log("hi");
+        //console.log(res.data.analytics);
         var labellist = [res.data.analytics[0].name,res.data.analytics[1].name,res.data.analytics[2].name];
         var qval = [res.data.analytics[0].total,res.data.analytics[1].total,res.data.analytics[2].total];
         this.setState({
@@ -65,10 +66,10 @@ export default class Analytics extends React.Component {
     let userLoginToken = localStorage.getItem("loginToken");
     let workspaceID = localStorage.getItem("currWorkspaceID");
     axios.get(`/api/workspaces/${workspaceID}/inventory`, {}).then(res => {
-        console.log(workspaceID);
-        console.log(userLoginToken);
+        //console.log(workspaceID);
+        //console.log(userLoginToken);
         //console.log(res.analytics);
-        console.log("hi");
+        //console.log("hi");
         var labellist = [];
         var qval = [];
         for (let i = 0; i<res.data.inventory.items.length; i++) {
@@ -83,6 +84,139 @@ export default class Analytics extends React.Component {
             datasets:[
               {
                 label: 'Food Item',
+                data: qval,
+                backgroundColor:[
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)',
+                  'rgba(255, 99, 132, 0.6)',
+                  'rgba(54, 162, 235, 0.6)',
+                  'rgba(255, 206, 86, 0.6)',
+                  'rgba(75, 192, 192, 0.6)',
+                  'rgba(153, 102, 255, 0.6)',
+                  'rgba(255, 159, 64, 0.6)',
+                  'rgba(139, 69, 19, 0.6)'
+                ]
+              }
+            ]
+          }
+        });
+        //console.log("doneq");
+    });
+  }
+
+  getExpChartData(){
+
+    let userLoginToken = localStorage.getItem("loginToken");
+    let workspaceID = localStorage.getItem("currWorkspaceID");
+    //console.log("letsgoooo");
+    axios.get(`/api/workspaces/${workspaceID}/analytics/expiration/expired`, { headers: { "Authorization" : `${userLoginToken}` } }).then(res => {
+        //console.log("letsgoooostart");
+        console.log(workspaceID);
+        console.log(userLoginToken);
+        console.log("hiexp");
+        console.log(res.data.analytics);
+        console.log(res.data);
+        console.log("hiexp");
+        let labellist = [];
+        let qval = [];
+        let counter = 0
+        for (let i = 0; i<res.data.analytics.length; i++) {
+          if(res.data.analytics[i].expired != 0){
+            labellist[counter] = res.data.analytics[i].name;
+            qval[counter] = res.data.analytics[i].total;
+            counter++;
+          }
+        }
+        //[res.data.analytics.inventory.items[0].name,res.data.analytics.inventory.items[1].name,res.data.analytics.inventory.items[2].name];
+        //[res.data.analytics.inventory.items[0].total,res.data.analytics.inventory.items[1].total,res.data.analytics.inventory.items[2].total];
+        this.setState({
+          chartExpData:{
+            labels: labellist,
+            datasets:[
+              {
+                label: 'Expired Food Items',
                 data: qval,
                 backgroundColor:[
                   'rgba(255, 99, 132, 0.6)',
@@ -224,7 +358,6 @@ export default class Analytics extends React.Component {
         <div class="Content">
         <h1>Analytics</h1>
         <Tabs>
-
           <TabList>
             <Tab><font face="Futura" size="4" color="grey"><b>Expiration Analytics</b></font></Tab>
             <Tab><font face="Futura" size="4" color="grey"><b>Quantity Analytics</b></font></Tab>
@@ -232,7 +365,7 @@ export default class Analytics extends React.Component {
           </TabList>
 
           <TabPanel>
-            <ExpirationChart chartData={this.state.chartDemoData} pantry="The Pantry" legendPosition="bottom"/>
+            <ExpirationChart chartData={this.state.chartExpData} pantry="The Pantry" legendPosition="bottom"/>
           </TabPanel>
           <TabPanel>
             <QuantityChart chartData={this.state.chartQuantData} pantry="The Pantry" legendPosition="bottom"/>
